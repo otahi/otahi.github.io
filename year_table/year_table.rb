d7dbdaadcc4ef09310cfd36ed203f55150ce982a
OTA Hiroshi <ota_h@nifty.com>
2014-02-08 16:21:37 +0900

    adds people images

--- year_table/year_table.rb
+++ year_table/year_table.rb
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 require "gviz"
 
+=begin
+    #template
+  ｛
+     src: , dst: , label: "",
+     img: "",
+     img_dl: "",
+     link: ""
+   },
+=end
+
 sections =
   [
    {
@@ -15,8 +25,16 @@ sections =
      img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Anderson%2C_Domenico_%281854-1938%29_-_n._23185_-_Socrate_%28Collezione_Farnese%29_-_Museo_Nazionale_di_Napoli.jpg/92px-Anderson%2C_Domenico_%281854-1938%29_-_n._23185_-_Socrate_%28Collezione_Farnese%29_-_Museo_Nazionale_di_Napoli.jpg",
      link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Anderson,_Domenico_(1854-1938)_-_n._23185_-_Socrate_(Collezione_Farnese)_-_Museo_Nazionale_di_Napoli.jpg"
    },
-   {src: -463, dst: -383, label: "シャカ" },
-   {src: -427, dst: -347, label: "プラトン" },
+   {src: -463, dst: -383, label: "シャカ",
+     img: "./images/Budda.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Kamakura_Budda_Daibutsu_front_1885.jpg/90px-Kamakura_Budda_Daibutsu_front_1885.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Kamakura_Budda_Daibutsu_front_1885.jpg"
+   },
+   {src: -427, dst: -347, label: "プラトン" ,
+     img: "images/plato.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Plato_Silanion_Musei_Capitolini_MC1377.jpg/80px-Plato_Silanion_Musei_Capitolini_MC1377.jpg",
+     link: "http://en.wikipedia.org/wiki/File:Plato_Silanion_Musei_Capitolini_MC1377.jpg"
+   },
    {src: -384, dst: -322, label: "アリストテレス" },
    {src: -287, dst: -212, label: "アルキメデス" },
    {src: -190, dst: -125, label: "ヒッパルコス" },
@@ -24,7 +42,16 @@ sections =
    {src:   83, dst:  168, label: "プトレマイオス" },
    {src:  570, dst:  632, label: "ムハンマド" },
    {src:  602, dst:  664, label: "玄奘(げんじょう)" },
-   {src: 1254, dst: 1324, label: "マルコ・ポーロ" },
+   {src: 1162, dst: 1272, label: "チンギス・ハン" ,
+     img: "images/Dschingis_Khan.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/3/35/YuanEmperorAlbumGenghisPortrait.jpg/94px-YuanEmperorAlbumGenghisPortrait.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:YuanEmperorAlbumGenghisPortrait.jpg"
+   },
+   {src: 1254, dst: 1324, label: "マルコ・ポーロ" ,
+     img: "images/Marco_Polo.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Marco_Polo_portrait.jpg/89px-Marco_Polo_portrait.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Marco_Polo_portrait.jpg"
+   },
    {
      src: 1412, dst: 1431, label: "ジャンヌダルク" ,
      img: "./Joan_of_arc_miniature_graded.jpg",
@@ -34,13 +61,29 @@ sections =
    {src: 1420, dst: 1506, label: "雪舟(せっしゅう)" },
    {src: 1451, dst: 1506, label: "コロンブス" },
    {src: 1452, dst: 1519, label: "レオナルド・ダ・ビンチ" },
-   {src: 1473, dst: 1543, label: "ニコラウス・コペルニクス" },
+   {src: 1473, dst: 1543, label: "ニコラウス・コペルニクス" ,
+     img: "images/Kopernikus.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Nikolaus_Kopernikus.jpg/103px-Nikolaus_Kopernikus.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Nikolaus_Kopernikus.jpg"
+   },
    {src: 1564, dst: 1642, label: "ガリレオ・ガリレイ" },
    {src: 1571, dst: 1630, label: "ヨハネス・ケプラー" },
    {src: 1642, dst: 1727, label: "アイザック・ニュートン" },
-   {src: 1732, dst: 1799, label: "ジョージ・ワシントン" },
-   {src: 1738, dst: 1822, label: "ウィリアム・ハーシェル" },
-   {src: 1750, dst: 1848, label: "カロライン・ハーシェル" },
+   {src: 1732, dst: 1799, label: "ジョージ・ワシントン",
+     img: "images/George_Washington.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/100px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
+   },
+   {src: 1738, dst: 1822, label: "ウィリアム・ハーシェル",
+     img: "images/William_Herschel01.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/3/36/William_Herschel01.jpg/98px-William_Herschel01.jpg",
+     link: "http://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:William_Herschel01.jpg"
+   },
+   {src: 1750, dst: 1848, label: "カロライン・ハーシェル",
+     img: "images/Herschel_Caroline_1829.jpg",
+     img_dl: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Herschel_Caroline_1829.jpg/97px-Herschel_Caroline_1829.jpg",
+     link: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Herschel_Caroline_1829.jpg/97px-Herschel_Caroline_1829.jpg"
+   },
    {src: 1787, dst: 1826, label: "ヨセフ・フォン・フラウンホーファー" },
    {src: 1792, dst: 1871, label: "ジョン・ハーシェル" },
    {src: 1803, dst: 1853, label: "ヨハン・クリスチャン・ドップラー" },
@@ -90,3 +133,4 @@ Graph do
   save(:year_table, :svg)
   save(:year_table, :png)
 end
+
